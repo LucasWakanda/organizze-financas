99de733568d45bf48ea615e3e1d4280a5ad16750
Lucas Wakanda <lucascorreiaaraujo77@gmail.com>
2023-03-27 11:54:19 -0300

    inicia projeto

---
+++ src/main/java/br/com/organizzefinancas/organizafinanca/OrganizaFinancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.organizzefinancas.organizafinanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizaFinancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizaFinancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/organizzefinancas/organizafinanca/OrganizaFinancaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.organizzefinancas.organizafinanca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizaFinancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
